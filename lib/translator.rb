cee188744b60a831e13124dab7877c58d455a22c
Marco Miranda <marmirand@hotmail.com>
2019-04-13 11:36:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 require "yaml"# require modules here
 
 def load_library
-  lib = YAML.load_file('emoticons.yml')# code goes here
+  library = YAML.load_file('lib/emoticons.yml')# code goes here
 end
 
 def get_japanese_emoticon
